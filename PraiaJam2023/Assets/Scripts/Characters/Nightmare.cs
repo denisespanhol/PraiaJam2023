011287349d3ce7734fffa440683964187f35212a
Denis Espanhol <denisgfespanhol@gmail.com>
2023-09-03 10:59:05 -0300

    Capturing the island name that the nightmare is on

--- PraiaJam2023/Assets/Scripts/Characters/Nightmare.cs
+++ PraiaJam2023/Assets/Scripts/Characters/Nightmare.cs
@@ -12,6 +12,7 @@ public class Nightmare : MonoBehaviour
     [SerializeField] private float nightmareSpeed = 2;
 
     private GameManager _gameManagerScript;
+    private GameObject _islandStaying;
 
     private int targetIndex = 0;
 
@@ -30,10 +31,12 @@ public class Nightmare : MonoBehaviour
         if (_gameManagerScript.activeBridge == null) return;
 
         GameObject activeBridge = _gameManagerScript.activeBridge;
+        GameObject[] rightPath;
 
-        if (activeBridge.name == firstBridge.name)
+        // Makes the nightmare walk to the first bridge when it's active
+        if (activeBridge.name == firstBridge.name && _islandStaying.name != "Dream Land 2")
         {
-            GameObject[] rightPath = paths[0].targetsToWalk;
+            rightPath = paths[0].targetsToWalk;
 
             if (targetIndex < rightPath.Length)
             {
@@ -44,7 +47,44 @@ public class Nightmare : MonoBehaviour
                 if (transform.position.x == targetPosition.x && transform.position.y == targetPosition.y) targetIndex += 1;
             }
 
-        } 
+        }
+
+        // Makes the nightmare walk to the second bridge when it's active
+        if (activeBridge.name == secondBridge.name)
+        {
+            rightPath = paths[1].targetsToWalk;
+
+            if (targetIndex < rightPath.Length)
+            {
+                Vector2 targetPosition = rightPath[targetIndex].transform.position;
+
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, nightmareSpeed * Time.deltaTime);
+
+                if (transform.position.x == targetPosition.x && transform.position.y == targetPosition.y) targetIndex += 1;
+            }
+
+        }
+
+        // Makes the nightmare walk to the second bridge when it's active
+        if (activeBridge.name == thirdBridge.name && _islandStaying.name != "Dream Land 1")
+        {
+            rightPath = paths[2].targetsToWalk;
+
+            if (targetIndex < rightPath.Length)
+            {
+                Vector2 targetPosition = rightPath[targetIndex].transform.position;
+
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, nightmareSpeed * Time.deltaTime);
+
+                if (transform.position.x == targetPosition.x && transform.position.y == targetPosition.y) targetIndex += 1;
+            }
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Island")) _islandStaying = collision.gameObject;
     }
 }
 
