4e378f152a13e4d356a8b4da1c44c16367d7c517
Denis Espanhol <denisgfespanhol@gmail.com>
2023-09-03 21:11:48 -0300

    Creating a closing time for a bridge withou any dream or nightmare to cross

--- PraiaJam2023/Assets/Scripts/GameManager.cs
+++ PraiaJam2023/Assets/Scripts/GameManager.cs
@@ -12,15 +12,34 @@ public class GameManager : MonoBehaviour
     public bool isDragging = false;
     public bool isABridgeActive = false;
 
+    [SerializeField] private float secondsToCloseTheBridge = 8f;
+
     private void Update()
     {
         checkIfAnyBridgeIsActive();
+        closeAllBridgesAfterATime();
     }
     private void checkIfAnyBridgeIsActive()
     {
         foreach(GameObject bridge in bridges)
         {
             if (bridge.activeInHierarchy) isABridgeActive = true;
+        }
+    }
+
+    private void closeAllBridgesAfterATime()
+    {
+        if (isABridgeActive) StartCoroutine(CloseTheBridge());
+    }
+
+    IEnumerator CloseTheBridge()
+    {
+        yield return new WaitForSeconds(secondsToCloseTheBridge);
+
+        foreach (GameObject bridge in bridges)
+        {
+            if (bridge.activeInHierarchy) bridge.SetActive(false);
+            isABridgeActive = false;
         }
     }
 }
